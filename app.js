60dfaab33f8b07dbe05b0579a8e261aa36dd7c52
Albet Matthew Best Nainggolan <golanbrother@gmail.com>
2023-06-19 08:36:51 +0700

    update

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
 app.use('/pengumuman', pengumumanRouter);
 app.use('/batch', batchRouter);
+
+
 module.exports = app;
